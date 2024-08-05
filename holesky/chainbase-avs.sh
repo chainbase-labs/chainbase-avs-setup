b506161beca538157eacd2ca531bf713c03aeb5a
Conan <12594210+0x0conan@users.noreply.github.com>
2024-08-05 16:07:17 +0800

    feat: add chainbase avs script(chainbase-avs.sh)
    ./chainbase-avs.sh register
    ./chainbase-avs.sh run
    ./chainbase-avs.sh stop

---
+++ holesky/chainbase-avs.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# path: chainbase-avs.sh
+
+. ./.env
+
+register_chainbase_avs() {
+  echo "Registering Chainbase AVS, ECDSA key path: $NODE_ECDSA_KEY_FILE_HOST"
+  docker run --env-file .env \
+    --rm \
+    --volume "${NODE_ECDSA_KEY_FILE_HOST}":"${NODE_ECDSA_KEY_FILE}" \
+    --volume "${NODE_LOG_PATH_HOST}":"${NODE_LOG_DIR}":rw \
+    "${MAIN_SERVICE_IMAGE}" \
+    $1
+}
+
+run_chainbase_avs() {
+  echo "Running Chainbase AVS"
+  docker compose up -d
+}
+
+stop_chainbase_avs() {
+  echo "Stopped Chainbase AVS"
+  docker compose down
+}
+
+print_help() {
+  echo "Usage: $0 {register|run|help}"
+  echo "Commands:"
+  echo "  register    Register the Chainbase AVS"
+  echo "  run         Run the Chainbase AVS"
+  echo "  help        Display this help message"
+}
+
+case "$1" in
+  register)
+    register_chainbase_avs $1
+    ;;
+  run)
+    run_chainbase_avs
+    ;;
+  stop)
+    stop_chainbase_avs
+    ;;
+  help)
+    print_help
+    ;;
+  *)
+    echo "Invalid command"
+    print_help
+    ;;
+esac
